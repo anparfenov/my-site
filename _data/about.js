bcb9c43119ed491543c4b8d781c0137b0b85f834
Andrey Parfenov <asleeppiano@outlook.com>
2022-08-23 23:16:39 +0300

    feat: adds computer specc

--- _data/about.js
+++ _data/about.js
@@ -20,6 +20,17 @@ module.exports = {
                 text: "dotfiles",
                 link: "https://github.com/moody-person/dotfiles"
             }
-        ]
+        ],
+        machine: {
+            link: 'https://www.ozon.ru/product/13-3-noutbuk-lenovo-ideapad-s540-13are-82dl003wru-amd-ryzen-7-4800u-1-8-ggts-ram-16-gb-ssd-512-gb-226665050/?sh=NWyGFZW8Wg',
+            name: 'Lenovo IdeaPad S540-13ARE',
+            spec: {
+                cpu: 'AMD Ryzen 7 4800U (1.8 ГГц)',
+                ram: '16 ГБ',
+                gpu: 'AMD Radeon Graphics',
+                ssd: '512 ГБ',
+                os: 'Windows 11 Home',
+            }
+        }
     }
 }
